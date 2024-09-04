bdb7ef062ad43b97efd1a6e09c84e8d2cd8c14cc
Hajin <qwertygoov@naver.com>
2024-09-04 23:13:50 +0900

    feat: 커스텀 로그인 필터 등록

--- ggg-authorization/src/main/java/org/example/gggauthorization/config/SecurityConfig.java
+++ ggg-authorization/src/main/java/org/example/gggauthorization/config/SecurityConfig.java
@@ -1,17 +1,31 @@
 package org.example.gggauthorization.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.gggauthorization.auth.CustomLoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    // AuthenticationManager 가 인자로 받을 authenticationConfiguration 객체, 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean // AuthenticationManager 빈 등록
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -33,6 +47,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/api/users/**").permitAll()
                         .anyRequest().authenticated());
+
+        /* 필터 등록 */
+        http
+                .addFilterAt(new CustomLoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
