2ba734655c0bbac92ee9a79f0126e4a1d4e76a53
Hajin <qwertygoov@naver.com>
2024-09-04 16:27:33 +0900

    feat: User Controller 정의 및 회원가입 컨트롤러 메소드 구현

---
+++ ggg-authorization/src/main/java/org/example/gggauthorization/controller/UserController.java
@@ -0,0 +1,22 @@
+package org.example.gggauthorization.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.gggauthorization.dto.UserJoinRequest;
+import org.example.gggauthorization.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public void joinUser(UserJoinRequest request) {
+        userService.joinUser(request);
+    }
+
+}
