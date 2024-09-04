8bd143a1c1188f8be05fe29a23ae71933f71cb42
Hajin <qwertygoov@naver.com>
2024-09-04 16:41:09 +0900

    add: Validation 의존성

--- ggg-authorization/src/main/java/org/example/gggauthorization/controller/UserController.java
+++ ggg-authorization/src/main/java/org/example/gggauthorization/controller/UserController.java
@@ -3,7 +3,9 @@ package org.example.gggauthorization.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.gggauthorization.dto.UserJoinRequest;
 import org.example.gggauthorization.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public void joinUser(UserJoinRequest request) {
+    public void joinUser(@RequestBody @Valid UserJoinRequest request) {
         userService.joinUser(request);
     }
 
