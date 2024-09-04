d3cad0da38c276adb4804f21c69dce90c02410f9
Hajin <qwertygoov@naver.com>
2024-09-04 16:25:10 +0900

    feat: User Service 정의 및 회원가입 서비스 메소드 구현

---
+++ ggg-authorization/src/main/java/org/example/gggauthorization/service/UserService.java
@@ -0,0 +1,31 @@
+package org.example.gggauthorization.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.gggauthorization.domain.entity.User;
+import org.example.gggauthorization.dto.UserJoinRequest;
+import org.example.gggauthorization.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void joinUser(UserJoinRequest request) {
+        String username = request.username();
+        String password = request.password();
+
+        boolean isExistedUser = userRepository.existsByUsername(username);
+        if (isExistedUser) {
+            return;
+        }
+
+        User newUser = User.builder()
+                .username(username)
+                .password(password)
+                .build();
+        userRepository.save(newUser);
+    }
+
+}
