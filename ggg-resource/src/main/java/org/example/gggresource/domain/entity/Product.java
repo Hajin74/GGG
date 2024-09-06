65476dfa1b1ca2f089a55a856b6f4323d8f4f09f
Hajin <qwertygoov@naver.com>
2024-09-06 20:30:56 +0900

    feat: Product(금) Entity 정의

---
+++ ggg-resource/src/main/java/org/example/gggresource/domain/entity/Product.java
@@ -0,0 +1,32 @@
+package org.example.gggresource.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.gggresource.enums.PurityType;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PurityType purityType;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal unitPrice;
+
+    @Column(nullable = false, length = 128)
+    private String title;
+
+    @Column(nullable = false, length = 512)
+    private String content;
+
+}
---
+++ ggg-resource/src/main/java/org/example/gggresource/enums/PurityType.java
@@ -0,0 +1,15 @@
+package org.example.gggresource.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum PurityType {
+
+    GOLD_999("99.9"),
+    GOLD_9999("99.99");
+
+    private final String purity;
+
+}
