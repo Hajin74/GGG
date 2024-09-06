cb79b7a04587ca487267316389df3207a31ac60b
Hajin <qwertygoov@naver.com>
2024-09-06 12:44:40 +0900

    feat: 로그인 성공 시, 다중 토큰 발급으로 변경

--- ggg-authorization/src/main/java/org/example/gggauthorization/controller/UserController.java
+++ ggg-authorization/src/main/java/org/example/gggauthorization/controller/UserController.java
@@ -4,21 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.example.gggauthorization.dto.UserJoinRequest;
 import org.example.gggauthorization.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+//@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/join")
+    @PostMapping("/api/users/join")
     public void joinUser(@RequestBody @Valid UserJoinRequest request) {
         userService.joinUser(request);
+    }
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "ok";
     }
 
 }
