450894fa520b6f0ceda0eccc936b4058d6f2411c
Hajin <qwertygoov@naver.com>
2024-09-08 21:21:06 +0900

    feat: 구매 주문 요청 dto 유효성 검사

--- ggg-resource/src/main/java/org/example/gggresource/controller/OrderController.java
+++ ggg-resource/src/main/java/org/example/gggresource/controller/OrderController.java
@@ -1,11 +1,13 @@
 package org.example.gggresource.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.gggresource.dto.OrderCreateRequest;
 import org.example.gggresource.dto.UserResponse;
 import org.example.gggresource.grpc.AuthServiceClient;
 import org.example.gggresource.service.OrderService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -21,7 +23,7 @@ public class OrderController {
      * todo: 주문 정보와 구매 타입에 맞는 인보이스가 생성됩니다.
      */
     @PostMapping("/buy")
-    public void createOrderBuy(@RequestHeader("accessToken") String accessToken, @RequestBody OrderCreateRequest orderCreateRequest) {
+    public void createOrderBuy(@RequestHeader("accessToken") String accessToken, @RequestBody @Validated OrderCreateRequest orderCreateRequest) {
         // 인증 서버에 토큰을 보내어 사용자 검증을 하고 사용자 정보를 응답값으로 받음
         UserResponse user = authServiceClient.authenticateUser(accessToken);
 
--- ggg-resource/src/main/java/org/example/gggresource/dto/OrderCreateRequest.java
+++ ggg-resource/src/main/java/org/example/gggresource/dto/OrderCreateRequest.java
@@ -1,11 +1,21 @@
 package org.example.gggresource.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.example.gggresource.enums.OrderType;
 
 public record OrderCreateRequest (
+        @NotNull(message = "상품 ID는 필수 입력 값입니다.")
+        @Positive
         Long productId,
+        @NotNull(message = "소비자 ID는 필수 입력 값입니다.")
+        @Positive
         Long customerId,
+        @NotNull(message = "주문 타입은 필수 입력 값입니다. (SELL/PURCHASE)")
         OrderType orderType,
+        @NotNull(message = "상품 수량은 필수 입력 값입니다.")
+        @Positive
         int quantity
 ) {
 
