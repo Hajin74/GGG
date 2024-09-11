7fbeccec470013810bb7d9cf397f90d4be8cc220
Hajin <qwertygoov@naver.com>
2024-09-11 21:30:00 +0900

    add: 초기화 data 넣기

---
+++ ggg-resource/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO product (id, unit_price, title, content, product_type, purity_type)
+VALUES (1, 12309000, '한국 골드바', '품질보증 합니다.', 'SELL', 'GOLD_9999');
+
+INSERT INTO product (id, unit_price, title, content, product_type, purity_type)
+VALUES (2, 12457000, 'MNM 골드바', '가치가 증명되었습니다.', 'SELL', 'GOLD_999');
+
+INSERT INTO product (id, unit_price, title, content, product_type, purity_type)
+VALUES (3, 11213300, '금 매입합니다.', '시세보다 높게 드립니다.', 'BUY', 'GOLD_999');
+
